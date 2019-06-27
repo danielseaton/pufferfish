f08b5094c76b7133292a3327ddd0864e900379b4
danielseaton <daniel.seaton87@gmail.com>
2019-06-27 07:15:50 +0100

    merge fix

--- pufferfish/dim_reduction.py
+++ pufferfish/dim_reduction.py
@@ -21,8 +21,8 @@ def pca_dim_reduction(m_df):
     pca.fit(m_df)
 
     #print(m_df.shape)
-    print("explained variance ration",pca.explained_variance_ratio_)
+    print("explained variance ratios: ", pca.explained_variance_ratio_)
 
     # pca_df = pd.DataFrame(data = pca.components_, index=['PC{}'.format(x) for x in range(1, n_components+1)], columns = m_df.columns)
 
-    pca_df.to_csv('../data/methylation_PCA.tsv', sep='\t')
+    return pca
